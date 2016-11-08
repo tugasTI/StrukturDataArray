eb05575f5feffbf6a240007a592b614cd5762810
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2016-11-09 01:24:31 +0700

    Sort sudah fix

--- com/data/Sort.java
+++ com/data/Sort.java
@@ -33,8 +33,10 @@ public class Sort{
 					}
 				}
 			}
-			else
+			else{
 				System.out.println("Maaf, pengurutan jenis "+tok[1]+" tidak tersedia.");
+				return data;
+			}
 
 		else if(tok[0].equals("nama"))
 			if(tok[1].equals("asc")){
@@ -61,9 +63,75 @@ public class Sort{
 					}
 				}
 			}
-			else
+			else{
 				System.out.println("Maaf, pengurutan jenis "+tok[1]+" tidak tersedia.");
+				return data;
+			}
+
+		else if(tok[0].equals("dist"))
+			if(tok[1].equals("asc")){
+				indexTerakhir = N-1;
+				for(int j=0; j<=indexTerakhir-1;j++){
+					for(int i=0; i<=indexTerakhir-1-j; i++){
+						if(data[i].distributor.compareTo(data[i+1].distributor) > 0){
+							newdata = data[i];
+							data[i]	= data[i+1];
+							data[i+1]	= newdata;
+						}
+					}
+				}
+			}
+			else if(tok[1].equals("desc")){
+				indexTerakhir = N-1;
+				for(int j=0; j<=indexTerakhir-1;j++){
+					for(int i=0; i<=indexTerakhir-1-j; i++){
+						if(data[i].distributor.compareTo(data[i+1].distributor) < 0){
+							newdata = data[i];
+							data[i]	= data[i+1];
+							data[i+1]	= newdata;
+						}
+					}
+				}
+			}
+			else{
+				System.out.println("Maaf, pengurutan jenis "+tok[1]+" tidak tersedia.");
+				return data;
+			}
 		
+		else if(tok[0].equals("harga"))
+			if(tok[1].equals("asc")){
+				indexTerakhir = N-1;
+				for(int j=0; j<=indexTerakhir-1;j++){
+					for(int i=0; i<=indexTerakhir-1-j; i++){
+						if(data[i].harga > data[i+1].harga){
+							newdata = data[i];
+							data[i]	= data[i+1];
+							data[i+1]	= newdata;
+						}
+					}
+				}
+			}
+			else if(tok[1].equals("desc")){
+				indexTerakhir = N-1;
+				for(int j=0; j<=indexTerakhir-1;j++){
+					for(int i=0; i<=indexTerakhir-1-j; i++){
+						if(data[i].harga < data[i+1].harga){
+							newdata = data[i];
+							data[i]	= data[i+1];
+							data[i+1]	= newdata;
+						}
+					}
+				}
+			}
+			else{
+				System.out.println("Maaf, pengurutan jenis "+tok[1]+" tidak tersedia.");
+				return data;
+			}
+
+		else{
+			System.out.println("Maaf, jenis data "+tok[0]+" tidak tersedia.");
+			return data;
+		}
 		System.out.println("Pengurutan data telah dilakukan, silakan periksa data pada menu tampil.");
 		return data;
 	}
